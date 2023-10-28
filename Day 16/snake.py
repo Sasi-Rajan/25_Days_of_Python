f11d5bcb40aa4f5bcc3071e1c08a2de7298ec544
Sasi <132489031+Sasi-Rajan@users.noreply.github.com>
2023-10-28 11:33:33 +0530

    Add files via upload

--- Day 16/snake.py
+++ Day 16/snake.py
@@ -1,44 +1,52 @@
 from turtle import Turtle
-SPEED=20
-UP=90
-DOWN=270
-LEFT=180
-RIGHT=0
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
+
     def __init__(self):
-        self.all_turtles=[]
+        self.segments = []
         self.create_snake()
-        
+        self.head = self.segments[0]
+
     def create_snake(self):
-        for x in range(0,60,20):
-            turtle=Turtle("square")
-            turtle.color("white")
-            turtle.penup()
-            turtle.goto(x=x,y=0)
-            self.all_turtles.append(turtle)
-            
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
-        for segment in range(len(self.all_turtles)-1,0,-1):
-            new_x=self.all_turtles[segment-1].xcor()
-            new_y=self.all_turtles[segment-1].ycor()
-            self.all_turtles[segment].goto(new_x,new_y)
-        self.all_turtles[0].forward(SPEED)
-        
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
     def up(self):
-        if self.all_turtles[0].heading() != DOWN:
-            self.all_turtles[0].setheading(UP)
-        
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
     def down(self):
-        if self.all_turtles[0].heading() != UP:
-            self.all_turtles[0].setheading(DOWN)
-        
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
     def left(self):
-        if self.all_turtles[0].heading() != RIGHT:
-            self.all_turtles[0].setheading(LEFT)
-        
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
     def right(self):
-        if self.all_turtles[0].heading() != LEFT:
-            self.all_turtles[0].setheading(RIGHT)
-    
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
